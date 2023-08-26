8ea04cc00056d3865fcd9dad3e98c71d547503cc
ElectroNick <nick@dsd.dev>
2023-08-25 19:20:02 -0500

    in process of fixing issue # 21. almost done

--- main/keyboard_config.h
+++ main/keyboard_config.h
@@ -100,7 +100,7 @@
 #define MAX_LAYER (LAYERS-1)
 #define MOD_LED_BYTES 2 //bytes for led status and modifiers
 #define MACRO_LEN 5 //keys for macros
-#define MACROS_NUM 36 //Available macro lenght
+#define MACROS_NUM 17 //Available macro lenght
 #define USER_MACROS_NUM 200
 #define USER_MACRO_NAME_LEN 10
 
--- main/keymap.c
+++ main/keymap.c
@@ -43,11 +43,9 @@ enum layer_holds
 
 enum custom_macros
 {
-	KC_CTRLALT = MACRO_BASE_VAL,	 // Ubuntu windows manager (change workspace)
-	KC_CTRLALTSHIFT,				 // Ubuntu windows manager (move windows)
-	KC_APP_SHUTTER,					 // Shutter
-	KC_APP_TERMINAL,				 // Terminal
-	KC_ALT_TAB,						 // Alt Tab
+	KC_APP_COPY = MACRO_BASE_VAL,	 // Copy
+	KC_APP_PASTE,					 // Paste
+	KC_ALT_TAB,	 					 // Alt Tab				 
 	KC_ALT_SHIFT_TAB,				 // Alt Shift Tab
 	KC_APP_CHROME_NTAB,				 // Chrome - Next tab
 	KC_APP_CHROME_PTAB,				 // Chrome - Prev tab
@@ -62,24 +60,7 @@ enum custom_macros
 	KC_APP_CHROME_REOPEN_TABS,		 // Chrome - Reopen all the tabs
 	KC_APP_CHROME_BACKWARD,			 // Chrome - Navigate Backward
 	KC_APP_CHROME_FORWARD,			 // Chrome - Navigate forward
-	KC_APP_WINDOWPUT_LINUX_K,		 // Windows plugin call windowput to move windows to certain parts. use this plus arrows.
-	KC_APP_GIMP_DESELECT,			 // Gimp shortcut to deselect all.
-	KC_APP_GIMP_INVERT,				 // Gimp shortcut to invert selection.
-	KC_APP_GIMP_FIT_IMAGE,			 // Gimp shortcut to fir image to window.
-	KC_APP_VSCODE_TOGGLE_SIDEBAR,	 // VSCode: Toggle sidebar.
-	KC_APP_VSCODE_COMMENT_LINE,		 // VSCode: Toggle line comment.
-	KC_APP_VSCODE_COMMENT_SELECTION, // VSCode: Toggle selection comment.
-	KC_APP_VSCODE_MULT_SELECTION,	 // VSCode: Multiple selection
-	KC_APP_VSCODE_FORWARD,			 // VSCode: Go forward
-	KC_APP_VSCODE_BACKWARD,			 // VSCode: Go Backward
-	KC_APP_VSCODE_FIND,				 // VSCode: Find
-	KC_APP_VSCODE_FIND_ALL,			 // VSCode: Find all
-	KC_APP_VSCODE_UNDO,				 // VSCode: undo
-	KC_APP_VSCODE_REDO,				 // VSCode: redo
-
-	KC_APP_COPY,	 // Copy
-	KC_APP_PASTE,	 // Paste
-	KC_SPECIAL_LINK, // SPECIAL LINK
+	
 };
 
 /*define what the macros do
@@ -87,14 +68,12 @@ enum custom_macros
  */
 uint16_t macros[MACROS_NUM][MACRO_LEN] = {
 	
-	// KC_CTR + ALT - //Ubuntu windows manager (change workspace)
-	{KC_LCTRL, KC_LALT, KC_NO},
-	// KC_CTR + ALT + Shift - Ubuntu windows manager (move windows)
-	{KC_LCTRL, KC_LALT, KC_LSHIFT, KC_NO},
-	// Shutter
-	{KC_LALT, KC_LSHIFT, KC_S, KC_NO},
-	// Terminal
-	{KC_LCTRL, KC_LALT, KC_T, KC_NO},
+
+	//  Copy
+	{KC_LCTRL, KC_C, KC_NO},
+	//  Paste
+	{KC_LCTRL, KC_V, KC_NO},
+
 	// alt + Tab
 	{KC_LALT, KC_TAB, KC_NO},
 	// alt + Shift + Tab
@@ -127,57 +106,26 @@ uint16_t macros[MACROS_NUM][MACRO_LEN] = {
 	// Chrome - Navigate forward
 	{KC_LALT, KC_RIGHT, KC_NO},
 
-	// Linux - WindowPut plugin - MosaicWindow
-	{KC_LGUI, KC_LALT, KC_NO},
-
-	// GIMP - Select none
-	{KC_LCTRL, KC_LSHIFT, KC_A, KC_NO},
-	// GIMP - Invert Selection
-	{KC_LCTRL, KC_I, KC_NO},
-	// GIMP - Fit image
-	{KC_LCTRL, KC_LSHIFT, KC_J, KC_NO},
-
-	// VSCode: Toggle sidebar
-	{KC_LCTRL, KC_B, KC_NO},
-	// VSCode: Toggle line comment.
-	{KC_LCTRL, KC_SLASH, KC_NO},
-	// VSCode: Toggle selection comment.
-	{KC_LCTRL, KC_LSHIFT, KC_A, KC_NO},
-	// VSCode: Multiple selection
-	{KC_LCTRL, KC_D, KC_NO},
-	// VSCode: Go forward
-	{KC_LCTRL, KC_LALT, KC_MINUS, KC_NO},
-	// VSCode: Go Backward
-	{KC_LCTRL, KC_LSHIFT, KC_MINUS, KC_NO},
-	// VSCode: Find
-	{KC_LCTRL, KC_F, KC_NO},
-	// VSCode: Find all
-	{KC_LCTRL, KC_LSHIFT, KC_F, KC_NO},
-
-	// VSCode: undo
-	{KC_LCTRL, KC_Z, KC_NO},
-	// VSCode: redo
-	{KC_LCTRL, KC_Y, KC_NO},
-
-	//  Copy
-	{KC_LCTRL, KC_C, KC_NO},
-	//  Paste
-	{KC_LCTRL, KC_V, KC_NO},
-
-	{KC_LCTRL, KC_K, KC_NO},
-
 };
 
-char *default_macro_name[] = {"KC_CTRLALT", "KC_CTRLALTSHIFT", "KC_APP_SHUTTER", "KC_APP_TERMINAL", "KC_ALT_TAB",
-							  "KC_ALT_SHIFT_TAB", "KC_APP_CHROME_NTAB", "KC_APP_CHROME_PTAB", "KC_APP_CHROME_NEW_TAB",
-							  "KC_APP_CHROME_NEW_WINDOW", "KC_APP_CHROME_N_INC_WINDOW", "KC_APP_CHROME_CLOSE_TAB",
-							  "KC_APP_CHROME_TAB1", "KC_APP_CHROME_TAB2", "KC_APP_CHROME_TAB3", "KC_APP_CHROME_TAB_LAST",
-							  "KC_APP_CHROME_REOPEN_TABS", "KC_APP_CHROME_BACKWARD", "KC_APP_CHROME_FORWARD",
-							  "KC_APP_WINDOWPUT_LINUX_K", "KC_APP_GIMP_DESELECT", "KC_APP_GIMP_INVERT", "KC_APP_GIMP_FIT_IMAGE",
-							  "KC_APP_VSCODE_TOGGLE_SIDEBAR", "KC_APP_VSCODE_COMMENT_LINE", "KC_APP_VSCODE_COMMENT_SELECTION",
-							  "KC_APP_VSCODE_MULT_SELECTION", "KC_APP_VSCODE_FORWARD", "KC_APP_VSCODE_BACKWARD", "KC_APP_VSCODE_FIND",
-							  "KC_APP_VSCODE_FIND_ALL", "KC_APP_VSCODE_UNDO", "KC_APP_VSCODE_REDO", "KC_APP_COPY", "KC_APP_PASTE",
-							  "KC_SPECIAL_LINK"};
+char *default_macro_name[] = {	"Copy", 
+								"Paste", 
+								"AltTab",
+								"AtlSTab", 
+								"NTAB", 
+								"PTAB", 
+								"NEW_TAB",
+								"NEW_WIND", 
+								"INC_WIND", 
+								"CloseTab",
+								"Tab1", 
+								"Tab2", 
+								"Tab3", 
+								"TabN",
+								"REOPEN_TABS", 
+								"BACKWARD", 
+								"FORWARD", 
+								};
 
 // char *default_macro_name[] = {"macro_1","macro_2","macro_3","macro_4","macro_5","macro_6","macro_7","macro_8","macro_9",
 // 							  "macro_10","macro_11","macro_12","macro_13","macro_14","macro_15","macro_16","macro_17","macro_18","macro_19",
@@ -277,7 +225,7 @@ dd_layer layer3 =
 		.key_map_names = {{"dev", "fulscr", "prtscr", "layer"}, {"PTAB", "NTAB", "reopne", "close"}, {"back", "forw", "new", "incog"}, {"Tab1", "Tab2", "Tab3", "TabN"}},
 
 		// Knobs - {CW, CCW, Single Press, Long Press, Double press}
-		.left_encoder_map = {KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_APP_VSCODE_UNDO, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK},
+		.left_encoder_map = {KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK},
 		.right_encoder_map = {KC_APP_CHROME_FORWARD, KC_APP_CHROME_BACKWARD, KC_APP_CHROME_NEW_TAB, KC_APP_CHROME_NEW_WINDOW, KC_APP_CHROME_CLOSE_TAB},
 		// APDS9960 -  {UP, DOWN, LEFT, RIGHT, NEAR, FAR}
 		.gesture_map = {KC_APP_CHROME_CLOSE_TAB, 0, KC_APP_CHROME_NTAB, KC_APP_CHROME_PTAB, 0, 0},
