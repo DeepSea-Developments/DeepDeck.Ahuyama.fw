204be40378014bf523c84d175e0c450459942e8c
ElectroNick <nick@dsd.dev>
2023-10-23 10:09:44 -0500

    WIP. Added tapdance and modtap functionality. The actios are still hardcoded so we have to create an structure to hold the information, and also all the routines to store in NVS, but we are doing good so far!

--- components/apds9960/apds9960.c
+++ components/apds9960/apds9960.c
@@ -54,28 +54,23 @@ static float __powf(const float x, const float y)
 
 apds9960_handle_t apds9960_create(i2c_bus_handle_t bus, uint8_t dev_addr)
 {
-
 	apds9960_dev_t *sens = (apds9960_dev_t *)calloc(1, sizeof(apds9960_dev_t));
 	sens->i2c_dev = i2c_bus_device_create(bus, dev_addr,
 										  i2c_bus_get_current_clk_speed(bus));
 	if (sens->i2c_dev == NULL)
 	{
 		free(sens);
-
 		return NULL;
 	}
 	sens->dev_addr = dev_addr;
 	sens->timeout = APDS9960_TIMEOUT_MS_DEFAULT;
-
 	return (apds9960_handle_t)sens;
 }
 
 esp_err_t apds9960_delete(apds9960_handle_t *sensor)
 {
-
 	if (*sensor == NULL)
 	{
-
 		return ESP_OK;
 	}
 
@@ -83,72 +78,9 @@ esp_err_t apds9960_delete(apds9960_handle_t *sensor)
 	i2c_bus_device_delete(&sens->i2c_dev);
 	free(sens);
 	*sensor = NULL;
-
 	return ESP_OK;
 }
-//
-// esp_err_t apds9960_gesture_init(apds9960_handle_t sensor) {
-//
-//	apds9960_enable_gesture_engine(sensor, false);
-//	apds9960_enable_proximity_engine(sensor, false);
-//	apds9960_enable_color_engine(sensor, false);
-//	apds9960_enable_color_interrupt(sensor, false);
-//	apds9960_enable_proximity_interrupt(sensor, false);
-//	/* Set ENABLE register to 0 (disable all features) */
-//	apds9960_enable(sensor, false);
-//
-//	/* Set default values for ambient light and proximity registers */
-//	apds9960_set_adc_integration_time(sensor, DEFAULT_ATIME);
-//	apds9960_set_register(sensor, APDS9960_WTIME, DEFAULT_WTIME);
-//
-//	apds9960_set_register(sensor, APDS9960_PPULSE, DEFAULT_PROX_PPULSE);
-//	apds9960_set_register(sensor, APDS9960_POFFSET_UR, DEFAULT_POFFSET_UR);
-//	apds9960_set_register(sensor, APDS9960_POFFSET_DL, DEFAULT_POFFSET_DL);
-//	apds9960_set_register(sensor, APDS9960_CONFIG1, DEFAULT_CONFIG1);
-//
-//	apds9960_set_led_drive_boost(sensor, APDS9960_LEDDRIVE_100MA,
-//			APDS9960_LEDBOOST_100PCNT);
-//	apds9960_set_proximity_gain(sensor, DEFAULT_PGAIN);
-//	apds9960_set_ambient_light_gain(sensor, DEFAULT_AGAIN);
-//
-//	apds9960_set_proximity_interrupt_threshold(sensor, DEFAULT_PILT,
-//	DEFAULT_PIHT, DEFAULT_PERS);
-//
-//	apds9960_set_light_inthigh_threshold(sensor, DEFAULT_AIHT);
-//	apds9960_set_light_intlow_threshold(sensor, DEFAULT_AILT);
-//
-//	apds9960_set_register(sensor, APDS9960_PERS, DEFAULT_PERS);
-//	apds9960_set_register(sensor, APDS9960_CONFIG2, DEFAULT_CONFIG2);
-//	apds9960_set_register(sensor, APDS9960_CONFIG3, DEFAULT_CONFIG3);
-//
-//	apds9960_set_gesture_enter_thresh(sensor, DEFAULT_GPENTH);
-//	apds9960_set_gesture_exit_thresh(sensor, DEFAULT_GEXTH);
-//	apds9960_set_register(sensor, APDS9960_GCONF1, DEFAULT_GCONF1);
-//	apds9960_set_gesture_gain(sensor, APDS9960_GGAIN_4X);
-//	apds9960_set_gesture_LEDDrive(sensor, DEFAULT_GLDRIVE);
-//	apds9960_set_gesture_waittime(sensor, APDS9960_GWTIME_2_8MS);
-//
-//	apds9960_set_register(sensor, APDS9960_GOFFSET_U, DEFAULT_GOFFSET);
-//	apds9960_set_register(sensor, APDS9960_GOFFSET_D, DEFAULT_GOFFSET);
-//	apds9960_set_register(sensor, APDS9960_GOFFSET_L, DEFAULT_GOFFSET);
-//	apds9960_set_register(sensor, APDS9960_GOFFSET_R, DEFAULT_GOFFSET);
-//	apds9960_set_register(sensor, APDS9960_GPULSE, DEFAULT_GPULSE);
-//	apds9960_set_register(sensor, APDS9960_GCONF3, DEFAULT_GCONF3);
-//
-//	apds9960_set_gesture_Int_Enable(sensor, DEFAULT_GIEN);
-//
-////
-//	apds9960_enable_gesture_interrupt(sensor, true);
-//
-//	apds9960_enable_proximity_engine(sensor, true);
-//	apds9960_set_gesture_dimensions(sensor, APDS9960_DIMENSIONS_ALL);
-//	apds9960_set_gesture_fifo_threshold(sensor, APDS9960_GFIFO_4);
-//	apds9960_reset_counts(sensor);
-//	apds9960_set_gesture_pulse(sensor, APDS9960_GPULSELEN_32US, 8);
-//
-//	return apds9960_enable_gesture_engine(sensor, true);
-////	return ESP_OK;
-//}
+
 
 esp_err_t apds9960_gesture_init(apds9960_handle_t sensor)
 {
@@ -1665,17 +1597,8 @@ uint8_t apds9960_read_gesture2(apds9960_handle_t sensor)
 		{
 
 			apds9960_reset_counts(sensor);
-			//			apds9960_clear_interrupt(sensor);
-			///////
-			//			apds9960_enable_gesture_interrupt(sensor, false);
-			//			apds9960_set_gesture_Int_Enable(sensor, 0);
-			//			apds9960_enable_gesture_interrupt(sensor, true);
-			//			apds9960_set_gesture_Int_Enable(sensor, 1);
-
-			//			vTaskDelay(pdMS_TO_TICKS(200));
+			vTaskDelay(pdMS_TO_TICKS(200));
 			apds9960_gesture_init(sensor);
-			//////
-
 			return gestureReceived;
 		}
 		scan_counter++;
